a5d099cc85bae352a6fa9eac5c09a7bddaafd19e
Michael Carroll <michael@openrobotics.org>
2020-11-06 11:09:38 -0600

    Feedback cleanups
    Signed-off-by: Michael Carroll <michael@openrobotics.org>

--- graphics/src/ColladaExporter.cc
+++ graphics/src/ColladaExporter.cc
@@ -41,24 +41,10 @@ static void LogTinyXml2DocumentError(
   {
     warning += std::to_string(_doc.ErrorID()) + "): ";
 
-#ifdef TINYXML2_MAJOR_VERSION_GE_6
     const char * error1 = _doc.ErrorStr();
-#else
-    const char * error1 = _doc.GetErrorStr1();
-#endif
 
     if (error1)
       warning += "str1=" + std::string(error1);
-
-#ifndef TINYXML2_MAJOR_VERSION_GE_6
-    const char * error2 = _doc.GetErrorStr2();
-
-    if (error1 && error2)
-      warning += ", ";
-
-    if (error2)
-      warning += "str2=" + std::string(error2);
-#endif
   }
   else
   {
